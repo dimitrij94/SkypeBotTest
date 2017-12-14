84d92887b63da351a71dc3cac57389bab53451cd
dimitij94 <dim.kost.94@gmail.com>
2017-12-14 16:11:43 +0200

    initial commit

---
+++ src/main/java/com/example/skype/bot/SkypeBotTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.skype.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypeBotTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkypeBotTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/skype/bot/SkypeBotTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.skype.bot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkypeBotTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
